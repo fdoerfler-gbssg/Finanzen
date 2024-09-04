1f292f4a4fe4180c449a8228ceee92d3feeeeace
Florian.Doerfler <Florian.Doerfler@CL4GT5CG4043DY4>
2024-09-04 09:17:28 +0200

    Add project files.

---
+++ Bankkonto/Bankkonto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankkonto
+{
+    public class Bankkonto(int kontonummer)
+    {
+        // Zinse nicht in Prozent
+        public static double AktivZins { get; } = 0.001;
+        public static double PassivZins { get; } = 0.015;
+
+        private int KontoNummer { get; } = kontonummer;
+        public double Guthaben { get; private set; } = 0;
+
+        private double ZinsGuthaben = 0.0;
+
+        public void ZahleEin(double betrag)
+        {
+            Guthaben += betrag;
+        }
+
+        public void Beziehe(double betrag)
+        {
+            Guthaben -= betrag;
+        }
+
+        public void SchribeZinsGut(int anzTage)
+        {
+            if (Guthaben > 0.0)
+                ZinsGuthaben += Guthaben * AktivZins / 360 * anzTage;
+            else
+                ZinsGuthaben += Guthaben * PassivZins / 360 * anzTage;
+        }
+
+        public void SchliesseKontoAb()
+        {
+            Guthaben += ZinsGuthaben;
+        }
+    }
+}
---
+++ BankkontoTests/BankkontoTests.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bankkonto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankkonto.Tests
+{
+    [TestClass()]
+    public class BankkontoTests
+    {
+        [TestMethod()]
+        public void AnfangsGuthabenIstNull()
+        {
+            // arrange
+            Bankkonto leeresBankkonto = new Bankkonto(0);
+
+            // act
+            // assert
+            Assert.AreEqual(0, leeresBankkonto.Guthaben, 0);
+        }
+
+        [TestMethod()]
+        public void KannEinzahlen()
+        {
+            // arrange
+            Bankkonto konto = new Bankkonto(0);
+
+            // act
+            konto.ZahleEin(100.0);
+
+            // assert
+            Assert.AreEqual(100.0, konto.Guthaben, 0.001);
+        }
+
+        [TestMethod()]
+        public void KannBeziehen()
+        {
+            // arrange
+            Bankkonto konto = new(0);
+
+            // act
+            konto.Beziehe(100.0);
+
+            // assert
+            Assert.AreEqual(-100.0, konto.Guthaben, 0.001);
+        }
+
+        [TestMethod()]
+        public void KeinZinsAufLeeremKOnto()
+        {
+            // arrange
+            Bankkonto konto = new(0);
+
+            // act
+            konto.SchribeZinsGut(1);
+            konto.SchliesseKontoAb();
+
+            // assert
+            Assert.AreEqual(0.0, konto.Guthaben, 0.001);
+
+        }
+
+        [TestMethod()]
+        public void EinTagZinsWirdGutgeschriebenAberNichtGebucht()
+        {
+            // arrange
+            Bankkonto konto = new(0);
+            const double AnfangsBetrag = 100.0;
+
+            // act
+            konto.ZahleEin(AnfangsBetrag);
+            konto.SchribeZinsGut(1);
+
+            // assert
+            Assert.AreEqual(AnfangsBetrag, konto.Guthaben, 0.001);
+        }
+
+        [TestMethod()]
+        public void EinTagAktivZinsWirdGutgeschriebenUndBeiAbschlussGebucht()
+        {
+            // arrange
+            Bankkonto konto = new(0);
+            const double AnfangsBetrag = 100.0;
+
+            // act
+            konto.ZahleEin(AnfangsBetrag);
+            konto.SchribeZinsGut(1);
+            konto.SchliesseKontoAb();
+
+            // assert
+            Assert.AreEqual(AnfangsBetrag + (AnfangsBetrag * Bankkonto.AktivZins) / 360, konto.Guthaben, 0.001);
+        }
+
+        [TestMethod()]
+        public void ZweiTageAktivZinsWirdGutgeschriebenUndBeiAbschlussGebucht()
+        {
+            // arrange
+            Bankkonto konto = new(0);
+            const double AnfangsBetrag = 100.0;
+
+            // act
+            konto.ZahleEin(AnfangsBetrag);
+            konto.SchribeZinsGut(2);
+            konto.SchliesseKontoAb();
+
+            // assert
+            Assert.AreEqual(AnfangsBetrag + 2 * AnfangsBetrag * Bankkonto.AktivZins / 360, konto.Guthaben, 0.001);
+        }
+
+        [TestMethod()]
+        public void ZweimalEinTagAktivZinsWirdGutgeschriebenUndBeiAbschlussGebucht()
+        {
+            // arrange
+            Bankkonto konto = new(0);
+            const double AnfangsBetrag = 100.0;
+
+            // act
+            konto.ZahleEin(AnfangsBetrag);
+            konto.SchribeZinsGut(1);
+            konto.SchribeZinsGut(1);
+            konto.SchliesseKontoAb();
+
+            // assert
+            Assert.AreEqual(AnfangsBetrag + 2 * AnfangsBetrag * Bankkonto.AktivZins / 360, konto.Guthaben, 0.001);
+        }
+
+        [TestMethod()]
+        public void EinTagPassivZinsWirdGutgeschriebenUndBeiAbschlussGebucht()
+        {
+            // arrange
+            Bankkonto konto = new(0);
+            const double TestBetrag = 100.0;
+
+            // act
+            konto.Beziehe(TestBetrag);
+            konto.SchribeZinsGut(1);
+            konto.SchliesseKontoAb();
+
+            // assert
+            double ErwarteterBetrag = - TestBetrag - (TestBetrag * Bankkonto.PassivZins / 360);
+            Assert.AreEqual(ErwarteterBetrag, konto.Guthaben, 0.001);
+        }
+    }
+}
